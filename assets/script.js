53ca4b9f15c079d9f7b61e5d3b2fd665758e78ca
Vita Fomin <vita.bukhalo@gmail.com>
2023-04-03 13:57:05 -0400

    formated my js file

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,6 @@ var searchbtn = $(".search-btn");
 var cityContainer = $(".container-fluid");
 var previousHistory = $(".history");
 
-
 function getApi(event, city, willSave) {
   event.preventDefault();
 
@@ -27,7 +26,7 @@ function getApi(event, city, willSave) {
       today.children().eq(0).text(header);
       var cityList = data.list;
 
-      $("img").remove()
+      $("img").remove();
 
       for (var i = 0; i < cityList.length; i = i + 8) {
         var main = cityList[i].main;
@@ -37,10 +36,6 @@ function getApi(event, city, willSave) {
         var icon = cityList[i].weather[0].icon;
         console.log(icon);
 
-
-
-       
-        
         var img = $("<img>").attr(
           "src",
           "https://openweathermap.org/img/wn/" + icon + "@2x.png"
@@ -48,40 +43,38 @@ function getApi(event, city, willSave) {
 
         $(".card-body" + (i / 8 + 1)).append(img);
 
-
         if (i == 0) {
-          $(".card-text-main").text("Temp: " + temp)
+          $(".card-text-main").text("Temp: " + temp);
           $(".card-text-main2").text("Humidity: " + humidity);
           $(".card-text-main3").text("Wind Speed: " + windSpeed);
-          $(".day1-text-main1").text("Temp: " + temp)
+          $(".day1-text-main1").text("Temp: " + temp);
           $(".day1-text-main2").text("Humidity: " + humidity);
           $(".day1-text-main3").text("Wind Speed: " + windSpeed);
-          
+
           today.append(img.clone());
           day1.append(img);
         }
 
         if (i == 8) {
-          $(".day2-text-main1").text("Temp: " + temp)
+          $(".day2-text-main1").text("Temp: " + temp);
           $(".day2-text-main2").text("Humidity: " + humidity);
           $(".day2-text-main3").text("Wind Speed: " + windSpeed);
-
         }
 
         if (i == 16) {
-          $(".day3-text-main1").text("Temp: " + temp)
+          $(".day3-text-main1").text("Temp: " + temp);
           $(".day3-text-main2").text("Humidity: " + humidity);
           $(".day3-text-main3").text("Wind Speed: " + windSpeed);
         }
 
         if (i == 24) {
-          $(".day4-text-main1").text("Temp: " + temp)
+          $(".day4-text-main1").text("Temp: " + temp);
           $(".day4-text-main2").text("Humidity: " + humidity);
           $(".day4-text-main3").text("Wind Speed: " + windSpeed);
         }
 
         if (i === 32) {
-          $(".day5-text-main1").text("Temp: " + temp)
+          $(".day5-text-main1").text("Temp: " + temp);
           $(".day5-text-main2").text("Humidity: " + humidity);
           $(".day5-text-main3").text("Wind Speed: " + windSpeed);
         }
@@ -89,7 +82,6 @@ function getApi(event, city, willSave) {
         console.log(cityList[i]);
       }
 
-      
       // if theres is data, go grab it
       var savedCities = localStorage.getItem("cities") || "[]";
       console.log("Saved : ", savedCities);
@@ -98,27 +90,23 @@ function getApi(event, city, willSave) {
       var cityArr = JSON.parse(savedCities);
       console.log("Parsed : ", cityArr);
       console.log("Type : ", typeof cityArr); // --> this is JS Object (Array)
-      
+
       // then we can Add data to it
       cityArr.push(searchinput.val().trim());
       console.log("New Array: ", cityArr);
       // We need to re-save the data
-      if(willSave) {
-        saveCity(JSON.stringify(cityArr)); 
+      if (willSave) {
+        saveCity(JSON.stringify(cityArr));
       }
     });
 }
 
-searchbtn.on("click", function(event) {
-  getApi(event, searchinput.val().trim(), true)
-
-
+searchbtn.on("click", function (event) {
+  getApi(event, searchinput.val().trim(), true);
 });
 
 function saveCity(data) {
   console.log(searchinput.val());
-
-
 
   //localStorage.setItem(searchinput.val(), data);
   localStorage.setItem("cities", data);
@@ -136,28 +124,23 @@ function history() {
 
     var cityList = $("<button>").attr("class", "list");
     cityList.text(visitedArr[i]);
-    console.log("cityList: " + cityList.text())
+    console.log("cityList: " + cityList.text());
 
-    cityList.on("click", function(event) {
-      var city = cityList.text()
-      console.log("---------" + city)
-      
-      getApi(event, $(this).text(), false)
-      
-      
-    
-    }) 
+    cityList.on("click", function (event) {
+      var city = cityList.text();
+      console.log("---------" + city);
 
-    console.log(cityList)
+      getApi(event, $(this).text(), false);
+    });
+
+    console.log(cityList);
     // we need to empty/clear our container
 
     previousHistory.append(cityList);
   }
 }
 
-
 history();
-
 
 today.children().eq(0).text(dayjs().format("MM/DD/YYYY"));
 day1.children().eq(0).text(dayjs().add(1, "day").format("MM/DD/YYYY"));
